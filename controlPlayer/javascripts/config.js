ddc0adac0f2b9986265cdf94a8b247a451eecc49
Rafael <letsrider@gmail.com>
2013-04-07 15:32:46 -0300

    Implementação YepNope
    Para controlar callbacks e fallbacks. Scripts carregados de acordo com
    os testes.

--- controlPlayer/javascripts/config.js
+++ controlPlayer/javascripts/config.js
@@ -1,4 +1,4 @@
-//Configurações de servidor
+//Global config
 var CONFIG = {
 	hostname: ''
 };
--- masterPlayer/javascripts/config.js
+++ masterPlayer/javascripts/config.js
@@ -1,4 +1,52 @@
-//Configurações de servidor
+//Global config
+//=============
 var CONFIG = {
 	hostname: ''
 };
+
+var masterPlayer = {};
+
+//Load resources
+//==============
+
+//Mandatory
+yepnope([
+	'javascripts/vendor/jquery.js',
+	'javascripts/vendor/jquery.jplayer.min.js',
+	'javascripts/vendor/jplayer.playlist.min.js'
+]);
+
+//Core
+yepnope({
+	load: 'javascripts/masterPlayer.js',
+	callback: function(){
+		masterPlayer.playerInit();
+	}
+});
+
+//Have connection?
+yepnope({
+	test: navigator.onLine,
+	yep: {
+		'socket': 'javascripts/vendor/socket.io.js',
+		'qrcodecore': 'javascripts/vendor/qrcode.js',
+		'jqueryqrcode': 'javascripts/vendor/jquery.qrcode.js'
+	},
+	callback: {
+		'socket': function(){
+			masterPlayer.socketInit();
+		},
+		'jqueryqrcode': function(){
+			masterPlayer.qrCodeInit();
+		}
+	}
+});
+
+//Have FileReader?
+yepnope({
+	test: true,
+	yep: ['javascripts/masterPlayer.js'],
+	callback: function(){
+		masterPlayer.fileReaderInit();
+	}
+});
