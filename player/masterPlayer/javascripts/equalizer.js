8556ef45d1cda8f605d8e2713b1c1f090bc1c66e
Rafael <letsrider@gmail.com>
2013-09-01 17:20:27 -0300

    Change "filter" name to "frequency"

--- player/masterPlayer/javascripts/equalizer.js
+++ player/masterPlayer/javascripts/equalizer.js
@@ -12,98 +12,98 @@ $(function(){
 	var audioElement	= document.getElementsByTagName('audio')[0]
 	var audioSource 	= audioContext.createMediaElementSource( audioElement );
 	var qFactor 		= 1;	//1 <> 100
-	var filters 		= {};
+	var frequencies		= {};
 	
-	//Filters
+	//Frequencies
 	//-----------------------
 
 	//32Hz
-	filters['1'] 					= audioContext.createBiquadFilter();
-	filters['1'].frequency.value 	= 32;
-	filters['1'].type 				= 3;		
-	filters['1'].gain.value 		= 0;
-	filters['1'].Q.value 			= qFactor;
+	frequencies['1'] 					= audioContext.createBiquadFilter();
+	frequencies['1'].frequency.value 	= 32;
+	frequencies['1'].type 				= 3;		
+	frequencies['1'].gain.value 		= 0;
+	frequencies['1'].Q.value 			= qFactor;
 
 	//64Hz
-	filters['2']				=	 audioContext.createBiquadFilter();
-	filters['2'].frequency.value 	= 64;
-	filters['2'].type 				= 3;
-	filters['2'].gain.value 		= 0;
-	filters['2'].Q.value 			= qFactor;
+	frequencies['2']				=	 audioContext.createBiquadFilter();
+	frequencies['2'].frequency.value 	= 64;
+	frequencies['2'].type 				= 3;
+	frequencies['2'].gain.value 		= 0;
+	frequencies['2'].Q.value 			= qFactor;
 
 	//125Hz
-	filters['3']  					= audioContext.createBiquadFilter();
-	filters['3'].frequency.value 	= 125;
-	filters['3'].type 				= 3;
-	filters['3'].gain.value 		= 0;
-	filters['3'].Q.value 			= qFactor;
+	frequencies['3']  					= audioContext.createBiquadFilter();
+	frequencies['3'].frequency.value 	= 125;
+	frequencies['3'].type 				= 3;
+	frequencies['3'].gain.value 		= 0;
+	frequencies['3'].Q.value 			= qFactor;
 
 	//250Hz
-	filters['4']  					= audioContext.createBiquadFilter();
-	filters['4'].frequency.value 	= 250;
-	filters['4'].type 				= 5;
-	filters['4'].gain.value 		= 0;
-	filters['4'].Q.value 			= qFactor;
+	frequencies['4']  					= audioContext.createBiquadFilter();
+	frequencies['4'].frequency.value 	= 250;
+	frequencies['4'].type 				= 5;
+	frequencies['4'].gain.value 		= 0;
+	frequencies['4'].Q.value 			= qFactor;
 
 	//500Hz
-	filters['5']  					= audioContext.createBiquadFilter();
-	filters['5'].frequency.value 	= 500;
-	filters['5'].type 				= 5;
-	filters['5'].gain.value 		= 0;
-	filters['5'].Q.value 			= qFactor;
+	frequencies['5']  					= audioContext.createBiquadFilter();
+	frequencies['5'].frequency.value 	= 500;
+	frequencies['5'].type 				= 5;
+	frequencies['5'].gain.value 		= 0;
+	frequencies['5'].Q.value 			= qFactor;
 
 	//1KHz
-	filters['6']  					= audioContext.createBiquadFilter();
-	filters['6'].frequency.value 	= 1000;
-	filters['6'].type 				= 5;
-	filters['6'].gain.value 		= 0;
-	filters['6'].Q.value 			= qFactor;
+	frequencies['6']  					= audioContext.createBiquadFilter();
+	frequencies['6'].frequency.value 	= 1000;
+	frequencies['6'].type 				= 5;
+	frequencies['6'].gain.value 		= 0;
+	frequencies['6'].Q.value 			= qFactor;
 
 	//2KHz
-	filters['7']  					= audioContext.createBiquadFilter();
-	filters['7'].frequency.value 	= 2000;
-	filters['7'].type 				= 5;
-	filters['7'].gain.value 		= 0;
-	filters['7'].Q.value 			= qFactor;
+	frequencies['7']  					= audioContext.createBiquadFilter();
+	frequencies['7'].frequency.value 	= 2000;
+	frequencies['7'].type 				= 5;
+	frequencies['7'].gain.value 		= 0;
+	frequencies['7'].Q.value 			= qFactor;
 
 	//4KHz
-	filters['8']  					= audioContext.createBiquadFilter();
-	filters['8'].frequency.value 	= 4000;
-	filters['8'].type 				= 4;
-	filters['8'].gain.value 		= 0;
-	filters['8'].Q.value 			= qFactor;
+	frequencies['8']  					= audioContext.createBiquadFilter();
+	frequencies['8'].frequency.value 	= 4000;
+	frequencies['8'].type 				= 4;
+	frequencies['8'].gain.value 		= 0;
+	frequencies['8'].Q.value 			= qFactor;
 
 	//8KHz
-	filters['9']  					= audioContext.createBiquadFilter();
-	filters['9'].frequency.value 	= 8000;
-	filters['9'].type 				= 4;
-	filters['9'].gain.value 		= 0;
-	filters['9'].Q.value 			= qFactor;
+	frequencies['9']  					= audioContext.createBiquadFilter();
+	frequencies['9'].frequency.value 	= 8000;
+	frequencies['9'].type 				= 4;
+	frequencies['9'].gain.value 		= 0;
+	frequencies['9'].Q.value 			= qFactor;
 
 	//16KHz
-	filters['10']  					= audioContext.createBiquadFilter();
-	filters['10'].frequency.value 	= 16000;
-	filters['10'].type 				= 4;
-	filters['10'].gain.value 		= 0;
-	filters['10'].Q.value 			= qFactor;
+	frequencies['10']  					= audioContext.createBiquadFilter();
+	frequencies['10'].frequency.value 	= 16000;
+	frequencies['10'].type 				= 4;
+	frequencies['10'].gain.value 		= 0;
+	frequencies['10'].Q.value 			= qFactor;
 
 	//Connect destination
-	audioSource.connect(filters['1']);
-	filters['1'].connect(filters['2']);
-	filters['2'].connect(filters['3']);
-	filters['3'].connect(filters['4']);
-	filters['4'].connect(filters['5']);
-	filters['5'].connect(filters['6']);
-	filters['6'].connect(filters['7']);
-	filters['7'].connect(filters['8']);
-	filters['8'].connect(filters['9']);
-	filters['9'].connect(filters['10']);
-	filters['10'].connect(audioContext.destination);
+	audioSource.connect(frequencies['1']);
+	frequencies['1'].connect(frequencies['2']);
+	frequencies['2'].connect(frequencies['3']);
+	frequencies['3'].connect(frequencies['4']);
+	frequencies['4'].connect(frequencies['5']);
+	frequencies['5'].connect(frequencies['6']);
+	frequencies['6'].connect(frequencies['7']);
+	frequencies['7'].connect(frequencies['8']);
+	frequencies['8'].connect(frequencies['9']);
+	frequencies['9'].connect(frequencies['10']);
+	frequencies['10'].connect(audioContext.destination);
 
 	//Local storage
 	//-----------------------
-	if(!savedUserInfo.get('equalizer.filters')) {
-		savedUserInfo.set('equalizer.filters', {
+	if(!savedUserInfo.get('equalizer.frequencies')) {
+		savedUserInfo.set('equalizer.frequencies', {
 				1: 0,
 				2: 0,
 				3: 0,
@@ -129,28 +129,28 @@ $(function(){
 		step: 0.1
 	};
 
-	var savedFilters = savedUserInfo.get('equalizer.filters');
+	var savedFrequencies = savedUserInfo.get('equalizer.frequencies');
 
-	$.each(filters, function(i, filter){
+	$.each(frequencies, function(i, filter){
 		$('.eq-fq-slider', '.equalizer-sliders').eq(i - 1).slider($.extend({
 			slide: function(event, ui) {	
-				filters[i.toString()].gain.value = ui.value;
+				frequencies[i.toString()].gain.value = ui.value;
 			},
 			change: function(event, ui){
-				filters[i.toString()].gain.value = ui.value;
+				frequencies[i.toString()].gain.value = ui.value;
 
 				//Save
-				savedFilters[i.toString()] = ui.value;
-				savedUserInfo.set('equalizer.filters', savedFilters);
+				savedFrequencies[i.toString()] = ui.value;
+				savedUserInfo.set('equalizer.frequencies', savedFrequencies);
 			}
 		},sliderDefaultOptions));
 
-		if( savedFilters[i.toString()] != 0 ) {
-			$('.eq-fq-slider', '.equalizer-sliders').eq(i - 1).slider('value', savedFilters[i.toString()]);
+		if( savedFrequencies[i.toString()] != 0 ) {
+			$('.eq-fq-slider', '.equalizer-sliders').eq(i - 1).slider('value', savedFrequencies[i.toString()]);
 		}
 	});
 
-	//Reset FILTERS
+	//Reset frequencies
 	$('.equalizer-reset', '#equalizer').on('click', function(event) {
 		$('.eq-fq-slider', '.equalizer-sliders').slider('value', '0');
 		event.preventDefault();
