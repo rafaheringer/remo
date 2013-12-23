a5e928a12315a3bc74646cc872490c2596bec2e9
Rafael <letsrider@gmail.com>
2013-12-22 23:22:03 -0200

    BugFix: Linux draggable window
    On linux distros the main window is bugged on dragging. Removed the JS
    and implement the CSS property -webkit-app-region

--- player/masterPlayer/javascripts/chromeApp.js
+++ player/masterPlayer/javascripts/chromeApp.js
@@ -48,37 +48,4 @@ masterPlayer.chromeAppInit = function() {
 			window.close();
 		}, 600);
 	});
-
-	//Drag chrome methods
-	this.chromeApp.startDrag = function(){
-		this.isDragging = true;
-		$(document).on('mousemove.startDrag', function(event){
-			chrome.app.window.current().setBounds({
-				left: event.pageX + chrome.app.window.current().getBounds().left - masterPlayer.chromeApp.currentMousePos.x,
-				top: event.pageY + chrome.app.window.current().getBounds().top - masterPlayer.chromeApp.currentMousePos.y
-			});
-
-			event.preventDefault();
-			return false;
-		});
-	};
-
-	this.chromeApp.stopDrag = function(){
-		this.isDragging = false;
-		$(document).off('mousemove.startDrag');
-	};
-
-	//Drag
-	$('#chrome-drag')
-		.on('mousedown', function(event){
-			masterPlayer.chromeApp.currentMousePos = {x: event.pageX, y: event.pageY};
-			masterPlayer.chromeApp.currentBounds = chrome.app.window.current().getBounds();
-			masterPlayer.chromeApp.startDrag();
-		});
-
-	$(document)
-		.on('mouseup', function(){
-			if(masterPlayer.chromeApp.isDragging)
-				masterPlayer.chromeApp.stopDrag();
-		});
 };
